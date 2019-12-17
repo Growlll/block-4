ee8b7dd159864e2461f7fb54706a68ac229a1103
Growlll <vetal.morenko@mail.ru>
2019-12-17 13:00:33 +0300

    Add js. Show/hide elements

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 let brands = document.querySelector('.brands')
+let item = document.querySelectorAll('.brands-name')
 let items  = brands.children
 let button = document.querySelector('.show')
+let flag = true
+
 
 button.addEventListener('click', function () {
-	for (let i = 0; i < items.length; i++) {
-		items[i].classList.remove('hidden-mobile')
+
+	if (flag) {
+			for (let i = 0; i < items.length; i++) {
+					items[i].classList.remove('hidden')
+			}
+			button.innerHTML = 'Скрыть'
+			button.style.backgroundImage = 'url("img/icons/arrow-top.svg")'
+			flag = false
+	} else {
+		for (let i = 0; i < items.length; i++) {
+			items[i].classList.add('hidden')
+		}
+		button.innerHTML = 'Показать все'
+		button.style.backgroundImage = 'url("img/icons/arrow-bottom.svg")'
+		flag = true
 	}
 })
 
